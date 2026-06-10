5ccc8003aca79621960311ff37daf54bd98d7d26
JackelineGS <jackeline.garcia.serna@gmail.com>
2026-06-10 00:26:44 -0500

    Initial commit - Spring Boot project setup

---
+++ src/main/java/com/argos/proyectocuidadointegral/ProyectoCuidadoIntegralApplication.java
@@ -0,0 +1,13 @@
+package com.argos.proyectocuidadointegral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoCuidadoIntegralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoCuidadoIntegralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/argos/proyectocuidadointegral/ProyectoCuidadoIntegralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argos.proyectocuidadointegral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCuidadoIntegralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
