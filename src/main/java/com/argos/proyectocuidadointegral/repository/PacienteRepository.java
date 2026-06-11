c30a12e6857b0aa0299eef98be95a0137d56fd43
JackelineGS <jackeline.garcia.serna@gmail.com>
2026-06-11 10:48:43 -0500

    Se crearon las clases de service y parte de repository

---
+++ src/main/java/com/argos/proyectocuidadointegral/repository/PacienteRepository.java
@@ -0,0 +1,4 @@
+package com.argos.proyectocuidadointegral.repository;
+
+public class PacienteRepository {
+}
---
+++ src/main/java/com/argos/proyectocuidadointegral/service/AdopcionService.java
@@ -0,0 +1,4 @@
+package com.argos.proyectocuidadointegral.service;
+
+public class AdopcionService {
+}
