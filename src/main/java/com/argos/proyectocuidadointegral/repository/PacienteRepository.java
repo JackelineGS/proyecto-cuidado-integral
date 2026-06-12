8bc84287403d50b6edfa237ee8c5544446ccbaca
VicentDev <175661774+Vicent-deVv@users.noreply.github.com>
2026-06-12 10:53:11 -0500

    Pacinte repository cambiado

--- src/main/java/com/argos/proyectocuidadointegral/repository/PacienteRepository.java
+++ src/main/java/com/argos/proyectocuidadointegral/repository/PacienteRepository.java
@@ -3,8 +3,10 @@ package com.argos.proyectocuidadointegral.repository;
 import com.argos.proyectocuidadointegral.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
-public interface PacienteRepository extends JpaRepository<Paciente,Long> {
-
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    List<Paciente> findByNameContainingIgnoreCaseOrOwnerContainingIgnoreCase(String name, String owner);
+    List<Paciente> findBySpecies(String species);
 }
