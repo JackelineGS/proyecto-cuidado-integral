6c97fc861fddfbafe58a14d20a2814f72a0ab3ec
JackelineGS <jackeline.garcia.serna@gmail.com>
2026-06-12 11:13:54 -0500

    Se agrego el mapping para ClienteController y citaController

--- src/main/java/com/argos/proyectocuidadointegral/controller/CitaController.java
+++ src/main/java/com/argos/proyectocuidadointegral/controller/CitaController.java
@@ -1,4 +1,19 @@
 package com.argos.proyectocuidadointegral.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
 public class CitaController {
+
+    @GetMapping("/citas")
+    public String verCitas(Model model) {
+        model.addAttribute("citas", new ArrayList<>());
+        return "citas/lista";
+    }
 }
