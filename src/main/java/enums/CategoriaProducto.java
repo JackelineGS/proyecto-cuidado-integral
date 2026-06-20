78326130b69727a30ee927f890a2a28c94393eac
dekoiwnl <tuemail@gmail.com>
2026-06-20 03:12:22 -0500

    Se añaden enums para categoría, estado y unidad de medida del módulo petshop

---
+++ src/main/java/enums/CategoriaProducto.java
@@ -0,0 +1,12 @@
+package com.argos.proyectocuidadointegral.enums;
+
+public enum CategoriaProducto {
+    ALIMENTO,
+    ACCESORIO,
+    MEDICAMENTO,
+    HIGIENE,
+    JUGUETE,
+    ROPA,
+    CAMA,
+    TRANSPORTADORA
+}
---
+++ src/main/java/enums/EstadoProducto.java
@@ -0,0 +1,8 @@
+package com.argos.proyectocuidadointegral.enums;
+
+public enum EstadoProducto {
+    DISPONIBLE,
+    AGOTADO,
+    DESCONTINUADO,
+    EN_REPOSICION
+}
