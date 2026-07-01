c2a8a52a7e9d4667a5b2fd950e362628d6cbdd31
VicentDev <175661774+Vicent-deVv@users.noreply.github.com>
2026-06-30 19:03:10 -0500

    LandingController actualizado

--- src/main/java/com/argos/proyectocuidadointegral/controller/LandingController.java
+++ src/main/java/com/argos/proyectocuidadointegral/controller/LandingController.java
@@ -1,14 +1,27 @@
 package com.argos.proyectocuidadointegral.controller;
 
+import com.argos.proyectocuidadointegral.repository.MascotaRescatadaRepository;
+import com.argos.proyectocuidadointegral.repository.ProductoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class LandingController {
 
-    @GetMapping("/")
-    public String verLanding() {
-        return "public/landing"; // Apunta a templates/public/landing.html
-    }
+    @Autowired
+    private MascotaRescatadaRepository mascotaRepository;
 
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping("/")
+    public String verLanding(Model model) {
+
+        model.addAttribute("adoptionPets", mascotaRepository.findAll());
+        model.addAttribute("products", productoRepository.findAll());
+
+        return "public/landing";
+    }
 }
